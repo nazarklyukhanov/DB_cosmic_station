6c2eb680d8dbc71d6a5ef3c0cb66dcac8e2b9e4a
Nazar <124512962+nazarklyukhanov@users.noreply.github.com>
2024-04-01 17:56:26 +0300

    Update cosmic_model.sql

--- cosmic_model.sql
+++ cosmic_model.sql
@@ -1,23 +1,21 @@
 CREATE TABLE IF NOT EXISTS space_station (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    expedition_id BIGINT NOT NULL,
-    department_id BIGINT NOT NULL,
-    FOREIGN KEY (expedition_id) REFERENCES expedition (id) ON DELETE CASCADE,
-    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS expedition (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     goal TEXT,
-    object_of_expedition_id BIGINT NOT NULL,
-    FOREIGN KEY (object_of_expedition_id) REFERENCES object_of_expedition (id) ON DELETE CASCADE
+    space_station_id BIGINT NOT NULL,
+    FOREIGN KEY (space_station_id) REFERENCES space_station (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS object_of_expedition (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    expedition_id BIGINT NOT NULL,
+    FOREIGN KEY (expedition_id) REFERENCES expedition (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS expedition_employee (
@@ -37,18 +35,20 @@ CREATE TABLE IF NOT EXISTS employee (
 CREATE TABLE IF NOT EXISTS department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    equipment_id BIGINT NOT NULL,
-    room_id BIGINT NOT NULL,
-    FOREIGN KEY (equipment_id) REFERENCES equipment (id) ON DELETE CASCADE,
-    FOREIGN KEY (room_id) REFERENCES room (id) ON DELETE CASCADE
+    space_station_id BIGINT NOT NULL,
+    FOREIGN KEY (space_station_id) REFERENCES space_station (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS equipment (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    department_id BIGINT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS room (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    department_id BIGINT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
